0794c74a94ace8c479355f448f565e4b687c14c3
peris <perishkin05@gmail.com>
2025-12-06 19:53:29 +0200

    Query8

--- LINQ_practice-master/Program.cs
+++ LINQ_practice-master/Program.cs
@@ -222,13 +222,26 @@ namespace Practice_Linq
             //Query 8: Перетворити всі матчі Євро-2012 (UEFA Euro), які відбулися в Україні, на матчі з наступними властивостями:
             // MatchYear - рік матчу, Team1 - назва приймаючої команди, Team2 - назва гостьової команди, Goals - сума всіх голів за матч
 
-            var selectedGames = games; // допиши запит
+            var selectedGames = games
+                .Where(g => g.Tournament == "UEFA Euro" && g.Date.Year == 2012 && g.Country == "Ukraine")
+                .Select(g => new
+                {
+                    MatchYear = g.Date.Year,
+                    Team1 = g.Home_team,
+                    Team2 = g.Away_team,
+                    Goals = g.Home_score + g.Away_score
+                })
+                .ToList();
 
 
             // Результат
             Console.WriteLine("\n======================== QUERY 8 ========================");
 
             //foreach
+            foreach (var g in selectedGames)
+            {
+                Console.WriteLine($"{g.MatchYear} {g.Team1} - {g.Team2}, Goals: {g.Goals}");
+            }
 
         }
 
